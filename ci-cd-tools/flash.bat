f07e760ae22c9f7c1b362e0df9465e4cfdb85ecd
Adam <Adam.richard.rodwell@gmail.com>
2024-08-21 14:21:03 +1000

    change the cd-cd tool files fro my system

--- ci-cd-tools/flash.bat
+++ ci-cd-tools/flash.bat
@@ -4,7 +4,6 @@ rem
 
 setlocal
 
-
 if not [%2]==[] goto :get_args
 echo "Missing arguments"
 exit /b 1
@@ -13,7 +12,7 @@ exit /b 1
 
 set "sn=%1"
 set "image_file=%2"
-set "flash_tool=C:\Program Files\STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI"
+set "flash_tool=C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI"
 set "flash_start_addr=0x08000000"
 
 "%flash_tool%" --connect port=SWD sn=%sn% --download "%image_file%" %flash_start_addr% -hardRst
--- ci-cd-tools/flashi.bat
+++ ci-cd-tools/flashi.bat
@@ -6,8 +6,8 @@ setlocal
 set "build_type=Debug"
 if not [%1]==[] set "build_type=%1"
 
-set "ws_root=C:\Users\gene\Documents\Gene\proj\STM32CubeIDE\workspace_1.6.1\ci-cd-class-1"
-set "sn=066BFF575550897767162155"
-set "image_file=%ws_root%\%build_type%\ci-cd-class-1.bin"
+set "ws_root=C:\Users\adamr\STM32CubeIDE\workspace_1.14.0\Stm32Test"
+set "sn=066DFF495087534867084847"
+set "image_file=%ws_root%\%build_type%\Stm32Dev.bin"
 
 "%ws_root%\ci-cd-tools\flash.bat" %sn% "%image_file%"
